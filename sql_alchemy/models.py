c078f929ba45b192c1d7b1bc961c4c48c2a53571
q.thompson <q.thompson088@gmail.com>
2023-05-02 08:38:37 -0700

    Adds the junction table for the relationship between products and orders

--- sql_alchemy/models.py
+++ sql_alchemy/models.py
@@ -1,21 +1,26 @@
 from database import db #this might need to come from a different script?
 
-class UserAccount():
+order_product = db.Table("order_product",
+                          db.Column("order_id", db.Integer, db.ForeignKey("order.id")),
+                          db.Column("product_id", db.Integer, db.ForeignKey("product.id"))
+                          )
+
+class UserAccount(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    screen_name = db.Column(db.String)
+    name = db.Column(db.String)
     email = db.Column(db.String)
 
     def to_json(self):
         return {
             "id":self.id,
-            "name":self.screen_name,
+            "name":self.name,
             "email":self.email,
         }
 
-class Order():
+class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.ForeignKey("useraccount.id"), primary_key=True)
-    products = db.relationship("OrderLines", back_populates="order")
+    products = db.relationship("Product", secondary=order_product, backref="orders")
 
     def to_json(self):
         product_list = []
@@ -23,12 +28,15 @@ class Order():
         for product in self.products:
             product_list.append({"name":product.name, "quantity":product.quantity})
             price_list.append(product.product.price * product.quantity)
+        
+        return {
+            "id":self.id,
+            "user":self.user_id,
+            "products":product_list,
+            "price_total":round(sum(price_list), 2)
+        }
 
-class OrderLines(): #should just have an order id and a product id, no name or line id required
-    line_id = db.Column(db.Integer)
-    name = db.Column(db.ForeignKey("product.name"))
-
-class Product():
+class Product(db.Model):
     product_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
@@ -41,3 +49,10 @@ class Product():
             "price":self.price,
             "quantity":self.quantity,
         }
+
+# class OrderLines(db.Model): #should just have an order id and a product id, no name or line id required
+#     product_id = db.Column(db.ForeignKey("product.id"))
+#     order_id = db.Column(db.ForeignKey("order.id"))
+    
+#     product = db.relationship("Product")
+#     order = db.relationship("Order")
