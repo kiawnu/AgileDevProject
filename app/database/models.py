52998aa9a42e6444c23421e0bab2e362807e0ead
q.thompson <q.thompson088@gmail.com>
2023-05-05 10:27:31 -0700

    Merges sql_alchemy and main branches.

--- app/database/models.py
+++ app/database/models.py
@@ -6,12 +6,66 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
+    email = db.Column(db.String)
+    orders = db.relationship("Order", backref="user")
     auth = db.Column(db.Boolean, default=False)
     
     
     def __repr__(self):
         return f"{self.id}"
+    
     def get_id(self):
         return self.id
+    
     def __auth__(self):
         return self.auth
+    
+    def to_json(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "email":self.email,
+            "admin":self.is_admin,
+        }
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    
+    products = db.relationship("OrderLine", back_populates="order")
+
+    def to_json(self):
+        product_list = []
+        price_list = []
+        for product in self.products:
+            product_list.append({"name":db.session.get(Product, product.product_id).name, "quantity":product.quantity})
+            price_list.append(db.session.get(Product, product.product_id).price * product.quantity)
+        
+        return {
+            "id":self.id,
+            "user":self.user_id,
+            "products":product_list,
+            "price_total":round(sum(price_list), 2)
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "price":self.price,
+            "quantity":self.quantity,
+        }
+
+class OrderLine(db.Model):
+    product_id = db.Column(db.ForeignKey("product.id"), primary_key=True)
+    order_id = db.Column(db.ForeignKey("order.id"), primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    product = db.relationship("Product")
+    order = db.relationship("Order", back_populates="products")
