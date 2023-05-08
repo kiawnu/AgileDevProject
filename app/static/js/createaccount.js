bf7fcec125242478b0635bdeb830847c8fd697fd
markus-bcit <mafonso4@my.bcit.ca>
2023-05-08 12:26:27 -0700

    adds sprint2.md

--- app/static/js/createaccount.js
+++ app/static/js/createaccount.js
@@ -1,6 +1,7 @@
 const createAccountButton = document.getElementById('create-account-btn');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
+
 const routeToCreateAccount = () => {
   event.preventDefault();
   let dataArray = []
@@ -25,3 +26,27 @@ const routeToCreateAccount = () => {
 };
 
 createAccountButton.addEventListener('click', routeToCreateAccount);
+
+
+const routeToCreateAccount = () => {
+  event.preventDefault();
+  let dataArray = []
+  let dataObj = { name: '', price: '', quanitiy: }
+  dataObj.username = username.value
+  dataObj.password = password.value
+  console.log(dataObj)
+  fetch('/admin/store', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataObj) //{'name': 'apple tree', 'price': 1, 'quantity': 1}
+  })
+    .then(response => {
+      if (response.status == 200) {
+        window.location.href = '/admin/store'
+      }else{window.location.href = '/admin/store'}
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
---
+++ docs/sprint2.md
@@ -0,0 +1,24 @@
+Hi everyone, 
+
+Great job last week!
+
+Im sure you've seen the post that we do not have class today, so I will not be coming in. 
+
+Here is what I think we should start this week:
+
+- Finish the front screen UI 
+- create the store database
+	- this is mainly the data that will be stored
+- Change the Store front so it is somewhat different from the info page
+	- currently, they are the same pages 
+- Using JS, route users to the info page that they clicked on 
+- ^^^ but on the store page
+- Route users to the store page when clicked from the info page
+- ^^^ other way too 
+- Add to cart functionality
+- Create the admin/store page where items can be added, updated, deleted
+- API routes for create, update, delete
+
+Everyone should be able to do all but the last task.
+These will be added to the trello board so please add yourself to the task you'd like to complete
+
