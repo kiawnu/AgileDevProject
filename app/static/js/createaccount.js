130fa58fda422ba358f75f36884fda0374786a76
Kiki <kabdollahi347@gmail.com>
2023-05-23 17:29:14 -0700

    fixes issues with create account not working, and non logged in users having access to shopping cart on info page

--- app/static/js/createaccount.js
+++ app/static/js/createaccount.js
@@ -1,29 +1,31 @@
-const createAccountButton = document.getElementById('create-account-btn');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
+const createAccountButton = document.getElementById("create-account-btn");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 
 const routeToCreateAccount = () => {
   event.preventDefault();
-  let dataArray = []
-  let dataObj = { username: '', password: '' }
-  dataObj.username = username.value
-  dataObj.password = password.value
-  console.log(dataObj)
-};
-fetch('/createaccount', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(dataObj)
-})
-  .then(response => {
-    if (response.status == 200) {
-      window.location.href = '/'
-    }else{window.location.href = '/createaccount'}
+  let dataArray = [];
+  let dataObj = { username: "", password: "" };
+  dataObj.username = username.value;
+  dataObj.password = password.value;
+  console.log(dataObj);
+
+  fetch("/createaccount", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataObj),
   })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+    .then((response) => {
+      if (response.status == 200) {
+        window.location.href = "/";
+      } else {
+        window.location.href = "/createaccount";
+      }
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+};
 
-createAccountButton.addEventListener('click', routeToCreateAccount);
-
+createAccountButton.addEventListener("click", routeToCreateAccount);
